1fcc0debd5785f4d93744d2db1182c6a1266498b
Shira <shira@elitzur.net>
2016-09-13 12:29:21 +0300

    Login & SignUp activities

---
+++ app/src/main/java/com/cafeteria/cafeteria_client/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.cafeteria.cafeteria_client;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView signUpLinkTv = (TextView)findViewById(R.id.signUpLinkTv);
+        signUpLinkTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                LoginActivity.this.finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/cafeteria/cafeteria_client/SignUpActivity.java
@@ -0,0 +1,106 @@
+package com.cafeteria.cafeteria_client;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        /* Get the components from the UI and set listeners to them */
+        // The Email EditText - handle events of changes in the focus, validation check
+
+        emailEditText = (EditText)findViewById(R.id.input_email);
+        emailEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    EditText emailEdit = (EditText)v;
+                    String emailInput = emailEdit.getText().toString();
+                    if (emailInput.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+                        emailEdit.setError(getResources().getString(R.string.email_address_error));
+                    } else {
+                        emailEdit.setError(null);
+                    }
+                }
+            }
+        });
+
+        // The Password EditText - handle events of changes in the focus, validation check
+
+        passwordEditText = (EditText)findViewById(R.id.input_password);
+        passwordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    EditText passwordEdit = (EditText)v;
+                    String passwordInput = passwordEdit.getText().toString();
+                    if (passwordInput.isEmpty() || passwordInput.length() < 6 || passwordInput.length() > 10 ) {
+                        passwordEdit.setError(getResources().getString(R.string.new_password_error));
+                    } else {
+                        passwordEdit.setError(null);
+                    }
+                }
+            }
+        });
+
+        confirmPasswordET = (EditText)findViewById(R.id.input_confirm_password);
+        confirmPasswordET.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                EditText inputEdit = (EditText)v;
+                String inputString = inputEdit.getText().toString();
+                String passwordString = passwordEditText.getText().toString();
+
+                if(!inputString.equals(passwordString)) {
+                    inputEdit.setError(getResources().getString(R.string.passwords_no_match_error));
+                }
+            }
+        });
+
+        // The SignUp Button - handle the event of click on the button
+
+        Button signUpBtn;
+        signUpBtn = (Button)findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if all the one of the input fields is not valid so the sign up is not authorized
+                if( emailEditText.getError() != null || passwordEditText.getError() != null ||
+                        emailEditText.getText().toString().isEmpty() ||
+                        passwordEditText.getText().toString().isEmpty()) {
+                    return;
+                }
+
+                // TODO: 13/09/2016  Add a real signUp logic (save details in db) and think about and fix the validation logic
+
+                Intent intent = new Intent(SignUpActivity.this, MenuActivity.class);
+                startActivity(intent);
+                SignUpActivity.this.finish();
+            }
+        });
+
+        TextView loginLinkTv = (TextView)findViewById(R.id.loginLinkTv);
+        loginLinkTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+                SignUpActivity.this.finish();
+            }
+        });
+    }
+}
